72b91eaa825160e1921d3224139ab53023124a63
Mateo <mateo.pavlovic@degordian.com>
2026-07-28 15:54:25 +0200

    fix: stabilize mobile newsletter focus

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'INGBIRO_VERSION', '1.6.13' );
+define( 'INGBIRO_VERSION', '1.6.14' );
 
 function ingbiro_setup() {
 	load_theme_textdomain( 'ingbiro', get_template_directory() . '/languages' );
