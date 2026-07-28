eb842bbc573a248e12415bdc1047fe1ca7630ed1
Mateo <mateo.pavlovic@degordian.com>
2026-07-28 14:38:14 +0200

    fix: refine responsive footer and event CTA

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'INGBIRO_VERSION', '1.6.6' );
+define( 'INGBIRO_VERSION', '1.6.8' );
 
 function ingbiro_setup() {
 	load_theme_textdomain( 'ingbiro', get_template_directory() . '/languages' );
